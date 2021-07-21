eada0e121c51cd187e197d6f8da8451d3516b3c5
JL Fisher <JamesLFisher@gmail.com>
2021-07-21 08:54:53 -0400

    moved header logging to middleware

--- back-end-opt01/routes/api.js
+++ back-end-opt01/routes/api.js
@@ -1,28 +1,18 @@
 const Router = require('@koa/router');
 const router = new Router();
 
-function dait() {
-    var dayt = new Date();
-    return dayt.toISOString();
+function timestamp() {
+    var now = new Date();
+    return now.toISOString();
 }
 
 router.get('/api', (ctx) => {
     console.log('in api.js: GET');
 
-    console.log('---\\/--- ctx.request.headers ---\\/--- ' + dait() );
-    console.log(ctx.request.headers);
-    console.log('---/\\--- ctx.request.headers ---/\\---');
-
     ctx.body = { a: 1, b: "two", c: [ "side", "sick" ] };
     // from https://akhromieiev.com/tutorials/how-to-send-json-response-using-koa/
     // if ctx.body is a string, Content-Type will automatically be set to "text/plain"
     // if ctx.body is on object or ray, Content-Type will automatically be set to "application/json"
-
-    console.log('---\\/--- ctx ---\\/---');
-    // console.log(ctx);
-    console.log(ctx.request);
-    console.log('---/\\--- ctx ---/\\--- ' + dait() );
-
 });
 
 router.options('/api', (ctx) => {
@@ -31,13 +21,9 @@ router.options('/api', (ctx) => {
 
     // the body can be empty or anything you want
     // only the headers and the status code are important; the response body is ignored by the client
-    // ctx.body = { "datetime": dait() };	// this works
+    // ctx.body = { "datetime": timestamp() };	// this works
     ctx.body = '';	// good: this returns "HTTP/1.1 200 OK" and "Content-Length: 0"
     // ctx.body = null;	// bad: this returns "HTTP/1.1 204 No Content" with no Content-Length header
-
-    console.log('---\\/--- ctx ---\\/---');
-    console.log(ctx);	// shows request and response
-    console.log('---/\\--- ctx ---/\\--- ' + dait() );
 
     // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
     // https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/OPTIONS
--- back-end-opt01/routes/debug.js
+++ back-end-opt01/routes/debug.js
@@ -5,15 +5,6 @@ router.get('/debug', (ctx) => {
     console.log('in debug.js');
     var dait = new Date();
     ctx.body = '<H1>Debugging Info</H1>Hello world!! The current date and time is ' + dait.toISOString();
-
-    console.log('---\\/--- ctx.request.headers ---\\/---');
-    console.log(ctx.request.headers);
-    console.log('---/\\--- ctx.request.headers ---/\\---');
-
-    console.log('---\\/--- ctx.request ---\\/---');
-    console.log(ctx.request);
-    console.log('---/\\--- ctx.request ---/\\---');
-
 });
 
 module.exports = router;
